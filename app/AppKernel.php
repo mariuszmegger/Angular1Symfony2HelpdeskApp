30bcd4c83949948c216b69e13723c153085dd7e6
mariuszmegger <mariuszmegger@go2.pl>
2016-03-04 21:08:41 +0100

    Common bundle and FOS installation

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new HelpdeskFrontendBundle\HelpdeskFrontendBundle(),
             new HelpdeskBackendBundle\HelpdeskBackendBundle(),
+            new HelpdeskCommonBundle\HelpdeskCommonBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/HelpdeskCommonBundle/HelpdeskCommonBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace HelpdeskCommonBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class HelpdeskCommonBundle extends Bundle
+{
+}
