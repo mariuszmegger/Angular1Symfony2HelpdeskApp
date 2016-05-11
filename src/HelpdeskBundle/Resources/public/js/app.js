7258b7e8e9d4fbb3dcfc9e1705181d203e80e0da
unknown <meggermariusz@p033044w403-l8.net.pp>
2016-05-11 15:00:09 +0200

    Category layout

--- src/HelpdeskBundle/Resources/public/js/app.js
+++ src/HelpdeskBundle/Resources/public/js/app.js
@@ -3,14 +3,59 @@ $(document).ready(function(){
   $( ".toggle-sidebar" ).click(function() {
   $("#sidebar").toggleClass( "collapse-sidebar" );
 
-
-
 });
 });
-
 
 (function() {
     var app = angular.module('helpdeskModule', ['ngRoute']);
+    
+    app.config(['$routeProvider',function($routeProvider){
+        $routeProvider.
+                when('/dashboard', {
+                    templateUrl: 'bundles/templates/Dashboard/dashboard.html',
+                    controller: 'DashboardController'
+                }).
+                when('/users', {
+                    templateUrl: 'bundles/templates/Users/usersList.html',
+                    controller: 'UsersController'
+                }).
+                when('/units', {
+                    templateUrl: 'bundles/templates/Units/unitsList.html',
+                    controller: 'UnitsController'
+                }).
+                when('/support_lines', {
+                    templateUrl: 'bundles/templates/SupportLines/supportLinesList.html',
+                    controller: 'SupportLinesController'
+                })
+                .when('/operators', {
+                    templateUrl: 'bundles/templates/Operators/OperatorsList.html',
+                    controller: 'OperatorsController'
+                })
+                .when('/categories', {
+                    templateUrl: 'bundles/templates/Categories/categoriesList.html',
+                    controller: 'CategoriesController'
+                })
+                .when('/add_category', {
+                    templateUrl: 'bundles/templates/Categories/add_category.html',
+                    controller: 'CategoriesController'
+                })
+                .when('/edit_category', {
+                    templateUrl: 'bundles/templates/Categories/edit_category.html',
+                    controller: 'CategoriesController'
+                })
+                .when('/categories', {
+                    templateUrl: 'bundles/templates/Categories/CategoriesList.html',
+                    controller: 'CategoriesController'
+                })
+                .when('/settings', {
+                    templateUrl: 'bundles/templates/Settings/SettingsList.html',
+                    controller: 'SettingsController'
+                }).
+                        
+                otherwise({
+                    redirectTo: '/dashboard'
+                });
+    }]);
 
     app.controller('DashboardController', ['$scope', function ($scope) {
             $scope.test = 'Dashboard';
@@ -40,40 +85,7 @@ $(document).ready(function(){
             $scope.test = 'Settings';
 
         }]);
+    
 
-    app.config(['$routeProvider',function($routeProvider){
-        $routeProvider.
-                when('/dashboard', {
-                    templateUrl: 'bundles/templates/Dashboard/dashboard.html',
-                    controller: 'DashboardController'
-                }).
-                when('/users', {
-                    templateUrl: 'bundles/templates/Users/usersList.html',
-                    controller: 'UsersController'
-                }).
-                when('/units', {
-                    templateUrl: 'bundles/templates/Units/unitsList.html',
-                    controller: 'UnitsController'
-                }).
-                when('/support_lines', {
-                    templateUrl: 'bundles/templates/SupportLines/supportLinesList.html',
-                    controller: 'SupportLinesController'
-                })
-                .when('/operators', {
-                    templateUrl: 'bundles/templates/Operators/OperatorsList.html',
-                    controller: 'OperatorsController'
-                })
-                .when('/categories', {
-                    templateUrl: 'bundles/templates/Categories/CategoriesList.html',
-                    controller: 'CategoriesController'
-                })
-                .when('/settings', {
-                    templateUrl: 'bundles/templates/Settings/SettingsList.html',
-                    controller: 'SettingsController'
-                }).
-                        
-                otherwise({
-                    redirectTo: '/dashboard'
-                });
-    }]);
+   
 })();
