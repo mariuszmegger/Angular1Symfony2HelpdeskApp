655de77240930be1eb29e0047a565f1110677911
unknown <MeggerMariusz@p033044w403-l8.net.pp>
2016-05-25 14:12:12 +0200

    search and limit box

---
+++ src/HelpdeskBundle/Resources/public/js/direcives.js
@@ -0,0 +1,15 @@
+
+(function () {
+    var app = angular.module('helpdeskDirective', []);
+
+    // app.directive('myOnKeyDownCall', function () {
+    // return function (scope, element, attrs) {
+    //     element.bind("keydown keypress", function (event) {
+    //             scope.$apply(function (){
+    //                 scope.$eval(attrs.ngEnter);
+    //             });
+    //             event.preventDefault();
+    //         });
+    //     };
+    // });
+})();
